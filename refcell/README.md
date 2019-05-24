afe1bdf666fe448e4ca144326ffe042ced0976cf
Gabriele <gsantomaggio@suse.com>
2019-05-24 18:57:31 +0200

    Add information to the example
    It should be easier to understand

--- refcell/README.md
+++ refcell/README.md
@@ -7,3 +7,70 @@ This is a reference cell example based on [Ra](https://github.com/rabbitmq/ra).
 ``` bash
 rebar3 compile
 ```
+
+
+## Test the example in localhost:
+
+
+To test the example in localhost with three nodes you can:
+
+- run the node1:
+```
+rebar3 shell --sname node1
+
+```
+
+- run the node2:
+```
+rebar3 shell --sname node1
+
+```
+- run the node2:
+```
+rebar3 shell --sname node1
+
+```
+
+Init the nodes with `refcell:init()`:
+
+```erlang
+(node1@GaS)1> refcell:init().
+2019-05-24T17:21:11.981891+02:00 notice: WAL: recovering ["/home/gabriele/git/Pivotal/ra/refcell/node1@GaS/00000030.wal"]
+2019-05-24T17:21:11.982120+02:00 notice: wal: opening new file "00000031.wal"
+{refcell,undefined,#{}}
+(node1@GaS)2> 2019-05-24T17:21:11.987432+02:00 notice: ra_log_segment_writer: error sending ra_log_event to: MYTE8K5Z7DX5I4A4. Error: No Pid
+2019-05-24T17:21:11.987524+02:00 notice: ra_log_segment_writer: cleaning closed table for 'MYTE8K5Z7DX5I4A4' range: 0-1
+2019-05-24T17:21:11.987598+02:00 notice: segment_writer: deleting wal file: 00000030.wal
+
+```
+
+Then start the cluster:
+
+```erlang
+ErlangNodes = [node1@GaS, node2@GaS, node3@GaS].
+refcell:start("My Test CLuster", ErlangNodes).
+```
+
+Check the cluster members:
+
+```erlang
+refcell:members().
+Cluster Members:
+Leader:{refcell,node1@GaS}
+Followers:[{refcell,node2@GaS},{refcell,node3@GaS}]
+Nodes:[{refcell,node1@GaS},{refcell,node2@GaS},{refcell,node3@GaS}]
+```
+
+put a value:
+```erlang
+refcell:put({refcell,node2@GaS}, "MyValue").
+```
+
+get a value:
+```erlang
+refcell:get({refcell,node1@GaS}).
+```
+
+You can test the failover by stopping a node, change the value, restart the node and execute the get again
+
+
--- refcell/src/refcell.erl
+++ refcell/src/refcell.erl
@@ -4,118 +4,137 @@
 -behaviour(ra_machine).
 
 -export([
-         init/1,
-         apply/3,
+  init/1,
+  init/0,
+  apply/3,
 
-         put/2,
-         get/1,
-         watch/1,
+  put/2,
+  get/1,
+  watch/1,
 
-         start/1,
-         start/2,
-         
-         state_enter/2
-         ]).
+  start/1,
+  start/2,
+
+  members/0,
+  members/1,
+
+  state_enter/2
+]).
 
 -record(?MODULE, {value :: term(),
-                  watchers = #{} :: #{pid() => ok}}).
+  watchers = #{} :: #{pid() => ok}}).
 
 -opaque state() :: #?MODULE{}.
 
 -type command() :: {put, term()} | get | {watch | unwatch, pid()}.
 
 -export_type([
-              state/0,
-              command/0
-              ]).
+  state/0,
+  command/0
+]).
 
 init(_) ->
-    #?MODULE{}.
+  #?MODULE{}.
+
+init() ->
+  application:ensure_all_started(ra),
+  init(init).
 
 apply(_Meta, {put, Value},
-      #?MODULE{value = Value} = State) ->
-    %% no change
-    {State, ok, []};
+    #?MODULE{value = Value} = State) ->
+  %% no change
+  {State, ok, []};
 apply(#{index := Idx}, {put, Value},
-      #?MODULE{watchers = Watchers, value = OldValue} = State0) ->
-    %% notify all watchers of the change of value
-    Effects0 = maps:fold(
-                fun (P, _, Acc) ->
-                        [{send_msg, P, {refcell_changed, OldValue, Value}}
-                         | Acc]
-                end, [], Watchers),
-    State = State0#?MODULE{value = Value},
-    %% emit a release cursor effect every 1000 commands or so
-    %% (give or take the number of non state machine commands that ra
-    %% processes
-    Effects = case Idx rem 1000 of
-                  0 -> [{release_cursor, Idx, State} | Effects0];
-                  _ -> Effects0
-              end,
-    {State, ok, Effects};
+    #?MODULE{watchers = Watchers, value = OldValue} = State0) ->
+  %% notify all watchers of the change of value
+  Effects0 = maps:fold(
+    fun(P, _, Acc) ->
+      [{send_msg, P, {refcell_changed, OldValue, Value}}
+        | Acc]
+    end, [], Watchers),
+  State = State0#?MODULE{value = Value},
+  %% emit a release cursor effect every 1000 commands or so
+  %% (give or take the number of non state machine commands that ra
+  %% processes
+  Effects = case Idx rem 1000 of
+              0 -> [{release_cursor, Idx, State} | Effects0];
+              _ -> Effects0
+            end,
+  {State, ok, Effects};
 apply(_Meta, get, State = #?MODULE{value = Value}) ->
-    {State, Value, []};
+  {State, Value, []};
 apply(_Meta, {watch, Pid}, State = #?MODULE{watchers = Watchers}) ->
-    {State#?MODULE{watchers = Watchers#{Pid => ok}},
-     ok, [{monitor, process, Pid}]};
+  {State#?MODULE{watchers = Watchers#{Pid => ok}},
+    ok, [{monitor, process, Pid}]};
 apply(_Meta, {down, Pid, noconnection}, State) ->
-    %% noconnection doesn't mean the watcher process is actually down
-    %% to find out we monitor the node and re-issue monitors when the node
-    %% comes back (see nodeup)
-    {State, ok, [{monitor, node, node(Pid)}]};
+  %% noconnection doesn't mean the watcher process is actually down
+  %% to find out we monitor the node and re-issue monitors when the node
+  %% comes back (see nodeup)
+  {State, ok, [{monitor, node, node(Pid)}]};
 apply(_Meta, {down, Pid, _Reason},
-      #?MODULE{watchers = Watchers} = State) ->
-    %% clean up watchers when monitor triggers
-    {State#?MODULE{watchers = maps:remove(Pid, Watchers)}, ok, []};
+    #?MODULE{watchers = Watchers} = State) ->
+  %% clean up watchers when monitor triggers
+  {State#?MODULE{watchers = maps:remove(Pid, Watchers)}, ok, []};
 apply(_Meta, {nodeup, Node},
-      #?MODULE{watchers = Watchers} = State) ->
-    %% notify all watchers
-    Effects = maps:fold(
-                fun (P, _, Acc) when node(P) =:= Node ->
-                        [{monitor, proess, P} | Acc]
-                end, [], Watchers),
-    {State, ok, Effects};
+    #?MODULE{watchers = Watchers} = State) ->
+  %% notify all watchers
+  Effects = maps:fold(
+    fun(P, _, Acc) when node(P) =:= Node ->
+      [{monitor, proess, P} | Acc]
+    end, [], Watchers),
+  {State, ok, Effects};
 apply(_Meta, {nodedown, _}, State) ->
-    %% we need to handle the nodedown as well to avoid crashing
-    {State, ok, []}.
+  %% we need to handle the nodedown as well to avoid crashing
+  {State, ok, []}.
 
 
 %% when a server enters leader state we need to re-issue all monitors
 state_enter(leader, #?MODULE{watchers = Watchers}) ->
-    maps:fold(
-                fun (P, _, Acc) ->
-                        [{monitor, process, P} | Acc]
-                end, [], Watchers);
+  maps:fold(
+    fun(P, _, Acc) ->
+      [{monitor, process, P} | Acc]
+    end, [], Watchers);
 state_enter(_, _) ->
-    [].
+  [].
 
 %% API
 
 put(Server, Value) ->
-    case ra:process_command(Server, {put, Value}) of
-        {ok, _, _} -> ok;
-        Err -> Err
-    end.
+  case ra:process_command(Server, {put, Value}) of
+    {ok, _, _} -> ok;
+    Err -> Err
+  end.
 
 get(Server) ->
-    case ra:process_command(Server, get) of
-        {ok, Value, _} ->
-            {ok, Value};
-        Err -> Err
-    end.
+  case ra:process_command(Server, get) of
+    {ok, Value, _} ->
+      {ok, Value};
+    Err -> Err
+  end.
 
 watch(Server) ->
-    case ra:process_command(Server, {watch, self()}) of
-        {ok, _, _} -> ok;
-        Err -> Err
-    end.
+  case ra:process_command(Server, {watch, self()}) of
+    {ok, _, _} -> ok;
+    Err -> Err
+  end.
 
 start(Name) ->
-    start(Name, [node()]).
+  start(Name, [node()]).
 
 start(Name, Nodes) ->
-    Servers = [{Name, N} || N <- Nodes],
-    ra:start_cluster(Name, {module, ?MODULE, #{}}, Servers).
+  Servers = [{refcell, N} || N <- Nodes],
+  ra:start_cluster(Name, {module, ?MODULE, #{}}, Servers).
+
+members() ->
+  members(node()).
+
+members(Node) ->
+  case ra:members({refcell, Node}) of
+    {ok, Result, Leader} -> io:format("Cluster Members:~nLeader:~p~nFollowers:~p~n" ++
+    "Nodes:~p~n", [Leader, lists:delete(Leader, Result), Result]);
+    Err -> io:format("Cluster Status error: ~p", [Err])
+  end.
+
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
